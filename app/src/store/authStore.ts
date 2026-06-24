25d463bd24e1ee121b6fb2f67fefd451027f81a1
vedantsahoo <vedantvibhusahu1234567@gmail.com>
2026-06-25 02:40:16 +0530

    bug fixes

--- app/src/store/authStore.ts
+++ app/src/store/authStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import type { User } from '@/types';
-// import '@/utils/ved.jpg';
+
 interface AuthState {
   user: User | null;
   isAuthenticated: boolean;
